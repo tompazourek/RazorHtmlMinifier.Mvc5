1ba6b02f78eaf339565c79f06f48914841934656
Tom Pažourek <tom.pazourek@gmail.com>
2017-11-17 19:17:39 +0100

    Added sample MVC 5 project.

---
+++ src/RazorHtmlMinifier.Sample/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Mvc;
+
+namespace RazorHtmlMinifier.Sample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index() => View();
+    }
+}
---
+++ src/RazorHtmlMinifier.Sample/Global.asax.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace RazorHtmlMinifier.Sample
+{
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            RouteTable.Routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            RouteTable.Routes.MapRoute("Default", "{controller}/{action}", new { controller = "Home", action = "Index" });
+        }
+    }
+}
